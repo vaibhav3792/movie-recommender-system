87ba5bf01b0a44a28eeba92fa96d390ca9c8189e
vaibhav3792 <vaibhavgupta8807@gmail.com>
2026-06-09 08:13:56 +0530

    Added Notebook

--- app.py
+++ app.py
@@ -222,7 +222,7 @@ div[data-testid="stSpinner"] {
 """, unsafe_allow_html=True)
 
 # ── Config ───────────────────────────────────────────
-TMDB_API_KEY = os.environ["TMDB_API_KEY"]
+TMDB_API_KEY = os.environ["TMDB_API_K
 
 # ── Load Models ──────────────────────────────────────
 @st.cache_resource
